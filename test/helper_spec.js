454622d900f5ec9b7d892689eeab27961acb6d27
Blake Sager <blake.sager@activeprospect.com>
2018-01-16 10:53:26 -0600

    Finish helper tests

--- test/helper_spec.js
+++ test/helper_spec.js
@@ -8,6 +8,7 @@ describe('Helper', () => {
 
     ['list_ids', 'list_id', 'list_names', 'list_name'].forEach(key => {
       describe(`specified with ${key}`, () => {
+        
         it('should handle single value', () => {
           let vars = { [key] : 'foo' };
           assert.deepEqual(helper.getListUrlNames(vars), 'foo');
@@ -43,6 +44,83 @@ describe('Helper', () => {
           assert.deepEqual(helper.getListUrlNames(vars), 'my_list|bobs_list_unplugged|20151012');
         });
       });
+    });
+  });
+
+  describe('Base URL', () => {
+
+    after(() => {
+      process.env.NODE_ENV = 'test';
+    });
+
+    it('should get production url', () => {
+      process.env.NODE_ENV = 'production';
+      assert.equal(helper.getBaseUrl(), 'https://app.suppressionlist.com');
+    });
+
+    it('should get staging url', () => {
+      process.env.NODE_ENV = 'staging';
+      assert.equal(helper.getBaseUrl(), 'http://staging.suppressionlist.com');
+    });
+
+    it('should get development url', () => {
+      process.env.NODE_ENV = 'development';
+      assert.equal(helper.getBaseUrl(), 'http://suppressionlist.dev');
+    });
+
+    it('should use the production url when process.env.NODE_ENV is undefined', () => {
+      delete process.env.NODE_ENV;
+      assert.equal(helper.getBaseUrl(), 'https://app.suppressionlist.com');
+    });
+  });
+
+  describe('Validate', () => {
+
+    it('should require list_ids', () => {
+      assert.equal(helper.validate({}), 'a list name is required');
+    });
+
+    it('should require values',() => {
+      assert.equal(helper.validate({list_ids: 'foo'}), 'values must not be blank');
+    });
+
+    it('should require non empty string values',() => {
+      assert.equal(helper.validate({list_ids: 'foo', values: ''}), 'values must not be blank');
+    });
+
+    it('should require non null values',() => {
+      assert.equal(helper.validate({list_ids: 'foo', values: null}), 'values must not be blank');
+    });
+
+    it('should require values in typed fields', () => {
+      const lead = {email: types.email.parse('')};  // an empty lead.email (a String) is different than ''
+      assert.equal(helper.validate({list_ids: 'foo', values: lead.email}), 'values must not be blank');
+    });
+
+    it('should be satisfied with list_ids, values', () => {
+      assert.isUndefined(helper.validate({list_ids: 'foo', values: 'bar@baz.com'}));
+    });
+  });
+
+  describe('Request Headers', () => {
+
+    let headers = helper.getRequestHeaders('api_key');
+
+    it('should accept JSON', () => {
+      assert.equal(headers.Accept, 'application/json');
+    });
+
+    it('should set authorization header', () => {
+      assert.equal(headers.Authorization, 'Basic WDphcGlfa2V5');
+    });
+
+    it('should set content-type by default', () => {
+      assert.equal(headers['Content-Type'], 'application/json');
+    });
+
+    it('should not set content-type when told not to', () => {
+      headers = helper.getRequestHeaders('api_key', false);
+      assert.isUndefined(headers['Content-Type']);
     });
   });
 });
