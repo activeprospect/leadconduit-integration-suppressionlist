31ea1a9e948d93f54ef86aba4b3f1a40f167f511
Blake Sager <blake.sager@activeprospect.com>
2018-01-16 14:21:33 -0600

    Delete CS folders

--- src/ui/webpack.js
+++
@@ -1,9 +0,0 @@
-const webpack           = require('webpack'),
-      webpackMiddleware = require('webpack-dev-middleware'),
-      config            = require('./webpack.config.js');
-
-
-module.exports = webpackMiddleware(webpack(config), {
-  publicPath: config.output.publicPath,
-  lazy: true
-});
